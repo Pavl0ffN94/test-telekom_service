dce81d01fcdb1f0655c3a6e602904001dcdf8755
PAvl0ffN <pavl0ffn94@gmail.com>
2024-07-11 10:22:51 +0400

    add message box

--- app/components/messageBox/index.tsx
+++ app/components/messageBox/index.tsx
@@ -1,9 +1,9 @@
-'use client'
+'use client';
 
 import React, {type FC} from 'react';
 import styles from './style.module.css';
 import dayjs from 'dayjs';
-import {  CheckOutlined } from '@ant-design/icons';
+import {CheckOutlined} from '@ant-design/icons';
 
 interface IMessageBox {
   message: string;
@@ -11,17 +11,18 @@ interface IMessageBox {
   isMine: boolean;
 }
 
-export const MessageBox:FC<IMessageBox> = ({ message, timestamp, isMine }) => {
+export const MessageBox: FC<IMessageBox> = ({message, timestamp, isMine}) => {
   const formattedTime = dayjs(timestamp).format('h:mm A');
 
   return (
-     <div className={`${styles.message} ${isMine ? styles.mine : styles.their}`}>
+    <div className={`${styles.message} ${isMine ? styles.mine : styles.their}`}>
       <div className={`${styles.text} ${isMine ? styles.mineText : styles.theirText}`}>
-        <p className={styles.messageContent}>{message}</p>
-        <span className={styles.timestamp}>{formattedTime}</span>
-        {isMine && <CheckOutlined className={styles.icon} />}
+        <div className={styles.messageContent}>{message}</div>
+        <div className={styles.mesageInfo}>
+          <div className={styles.timestamp}>{formattedTime}</div>
+          {isMine && <CheckOutlined className={styles.icon} />}
+        </div>
       </div>
     </div>
   );
 };
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-
 import styles from './page.module.css';
 import {Header, MessageBox} from './components';
 
 const messages = [
-  { message: "I am down for whatever!", timestamp: "2024-07-10T11:36:00", isMine: true },
-  { message: "Sounds good to me!", timestamp: "2024-07-10T11:37:00", isMine: false },
+  {message: 'I am down for whatever!', timestamp: '2024-07-10T11:36:00', isMine: true},
+  {message: 'Sounds good to me!', timestamp: '2024-07-10T11:37:00', isMine: false},
+  {
+    message: 'Sounds good to me! asfasf asf sfasfasfasfwqfzfsadfqafasfwqfaf',
+    timestamp: '2024-07-10T11:37:00',
+    isMine: false,
+  },
 ];
 
 export default function Home() {
@@ -12,11 +16,10 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.container}>
         <Header />
-         <div>
-      {messages.map((msg, index) => (
-        <MessageBox key={index} {...msg} />
-      ))}
-    </div>
+
+        {messages.map((msg, index) => (
+          <MessageBox key={index} {...msg} />
+        ))}
       </div>
     </main>
   );
